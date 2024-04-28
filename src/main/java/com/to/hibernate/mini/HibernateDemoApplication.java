1b3f63207b5afc0a15f6c9669ff92b4ca16bf33a
Harshik Patel <harshikpatel2710@gmail.com>
2024-04-28 00:44:36 -0400

    Adding DeploymentService.yaml for k8s

--- src/main/java/com/to/hibernate/mini/HibernateDemoApplication.java
+++ src/main/java/com/to/hibernate/mini/HibernateDemoApplication.java
@@ -4,10 +4,18 @@ package com.to.hibernate.mini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HibernateDemoApplication {
 
+	@GetMapping
+	public String message(){
+		return "welcome to javatechie";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateDemoApplication.class, args);
 
